6c29f40f63363c49e6c0048bc95729e8594beb89
pranavchess2004 <86563668+pranavchess2004@users.noreply.github.com>
2021-06-27 22:26:06 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+/ src/Title.js
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>ShapeAI</h1>
+    </div>
+  )
+}
+
+export default Title
